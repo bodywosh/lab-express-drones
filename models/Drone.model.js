1511c8f8c9b7bbb50c4230819131282816343004
bodywosh <bodywashtv@gmail.com>
2022-03-22 11:05:39 +0100

    iteration 1

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
 // Iteration #1
+const { Schema, model } = require('mongoose')
+
+const schema = new Schema({
+    name: { type: String, unique: true },
+    propellers: Number,
+    maxSpeed: Number,
+  })
+  
+  const Drone = model('Drone', schema)
+  
+  module.exports = Drone
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,16 @@
 // Iteration #1
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+]
+
+  require('../db/index.js');
+  const Drone = require('../models/Drone.model.js')
+
+  const seedDB = async () => {
+    const nbDrones = await Drone.insertMany(drones)/// renvoie les items push
+    console.log(nbDrones.length," drones have been created")
+  }
+
+  seedDB()
